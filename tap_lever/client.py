6ac4fe89c96f0def2792a58768e585ff5995e85d
Manuel Ponsa <manuponsa@gmail.com>
2024-02-12 12:20:35 -0300

    fix: avoid raising when next page tokens are the same

--- tap_lever/client.py
+++ tap_lever/client.py
@@ -1,9 +1,11 @@
 """REST client handling, including LeverStream base class."""
 
-import requests, json
+import requests
+import json
 from pathlib import Path
 from typing import Any, Dict, Optional, Union, List, Iterable
 from urllib.parse import unquote
+import copy
 
 from memoization import cached
 
@@ -39,6 +41,54 @@ class LeverStream(RESTStream):
             params["updated_at_start"] = offsetVal[0]
         elif self.replication_key:
             filterDate = self.get_starting_timestamp(context)
-            timestampDate = int(filterDate.timestamp() * 1000) # Milliseconds
+            timestampDate = int(filterDate.timestamp() * 1000)  # Milliseconds
             params["updated_at_start"] = timestampDate
         return params
+
+    def get_next_page_token(
+        self, response: requests.Response, previous_token: Optional[Any]
+    ) -> Any:
+        response_json = response.json()
+
+        hasNext = response_json.get("hasNext", False)
+
+        if hasNext == False:
+            return None
+
+        if self.next_page_token_jsonpath:
+            all_matches = extract_jsonpath(
+                self.next_page_token_jsonpath, response.json()
+            )
+            first_match = next(iter(all_matches), None)
+
+            next_page_token = first_match
+        else:
+            next_page_token = response.headers.get("X-Next-Page", None)
+
+        if next_page_token == previous_token:
+            return None
+
+        return next_page_token
+
+
+def request_records(self, context: Optional[dict]) -> Iterable[dict]:
+    next_page_token: Any = None
+    finished = False
+    decorated_request = self.request_decorator(self._request)
+
+    while not finished:
+        prepared_request = self.prepare_request(
+            context, next_page_token=next_page_token
+        )
+        resp = decorated_request(prepared_request, context)
+        for row in self.parse_response(resp):
+            yield row
+        previous_token = copy.deepcopy(next_page_token)
+        next_page_token = self.get_next_page_token(
+            response=resp, previous_token=previous_token
+        )
+        if next_page_token and next_page_token == previous_token:
+            finished = True
+            return
+
+        finished = not next_page_token
